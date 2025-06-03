4fd78a8ee809bba4d0a8666198a8509d74b3890f
Alexander Prypyshnyi <alexpryp@gmail.com>
2025-06-03 12:13:02 +0300

    ADD | working with streams

--- src/lessonsWebDev/streams/app.js
+++ src/lessonsWebDev/streams/app.js
@@ -2,24 +2,37 @@ const fs = require('fs');
 const path = require('path');
 const zlib = require('zlib');
 
+// read and write streams
 const readStream = fs.createReadStream(path.resolve(__dirname, 'docs', 'text.txt'));
 const writeStream = fs.createWriteStream(path.resolve(__dirname, 'docs', 'new-text.txt'));
-const compressStream = zlib.createGzip();
-
-// // duplex stream
-// readStream.on('data', (chunk) => {
-//   writeStream.write('\n---CHUNK START---\n');
-//   writeStream.write(chunk);
-//   writeStream.write('\n---CHUNK END---\n');
-// });
 
 const handleError = () => {
   console.log('Error');
   readStream.destroy();
   writeStream.end('Finished with error...');
-}
+};
 
-// pipe stream
+// // readStream.on('data', (chunk) => {
+// //   writeStream.write('\n---CHUNK START---\n');
+// //   writeStream.write(chunk);
+// //   writeStream.write('\n---CHUNK END---\n');
+// // });
+
+// const handleError = () => {
+//   console.log('Error');
+//   readStream.destroy();
+//   writeStream.end('Finished with error...');
+// };
+
+// // pipe
+// readStream
+//   .on('error', handleError)
+//   .pipe(writeStream)
+//   .on('error', handleError);
+
+// transforming flow
+const compressStream = zlib.createGzip();
+
 readStream
   .on('error', handleError)
   .pipe(compressStream)
