85f45863decbdc49ecb3d3984b7a1c2d27fa9089
Alexander Prypyshnyi <alexpryp@gmail.com>
2025-06-11 17:36:56 +0300

    ADD | working with global objects

---
+++ src/lessonsWebDev/routing/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Server request');
+
+    res.setHeader('Content-Type', 'text/html');
+});
+
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
