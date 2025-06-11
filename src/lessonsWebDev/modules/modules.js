d12b96737d198c3c6404b8e0964750812bdfa52a
Alexander Prypyshnyi <alexpryp@gmail.com>
2025-06-11 18:14:42 +0300

    EDIT | modules lesson

--- src/lessonsWebDev/modules/modules.js
+++ src/lessonsWebDev/modules/modules.js
@@ -1,11 +1,13 @@
 const os = require('os');
-const {userName: user, sayHi} = require('../test/test.js');
+const { userName: user, sayHi } = require('./test');
+
+console.log(user);
 
 const name = 'Tommy';
 
-console.log(user);
 console.log(sayHi(name));
 
-console.log(os.platform(), os.release());
+console.log(os.platform());
+console.log(os.release());
 
 module.exports = name;
